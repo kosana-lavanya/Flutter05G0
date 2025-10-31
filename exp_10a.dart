60d57cba85a01790ed475cd82944784d573d6a7b
kosana-lavanya <lavanyakosana58@gmail.com>
2025-10-31 10:50:30 +0530

    Update exp_10a.dart

--- exp_10a.dart
+++ exp_10a.dart
@@ -1,11 +1,1 @@
-import 'package:flutter/material.dart';
-
-void main() => runApp(MaterialApp(home: Scaffold(
-  appBar: AppBar(title: const Text("Widgets")),
-  body: Column(children: [
-    const Text("Hello Flutter!"),
-    Image.network("https://flutter.dev/images/flutter-logo-sharing.png", width: 80),
-    Container(color: Colors.blue, padding: const EdgeInsets.all(8),
-      child: const Text("Container Text", style: TextStyle(color: Colors.white))),
-  ]),
-)));
+int add(int a, int b) => a + b;
